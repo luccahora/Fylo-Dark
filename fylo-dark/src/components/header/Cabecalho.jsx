6c228001885499f64f13d834686bc492766c82eb
Lucca Hora <luccahora@gmail.com>
2020-09-22 21:06:14 -0300

    header

---
+++ fylo-dark/src/components/header/Cabecalho.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import logo from '../../assets/logo.svg'
+import '../../assets/main.css'
+import './cabecalho.css'
+
+const cabecalho = () => {
+    return (
+      <header className="cabecalho">
+        <img src={logo} alt="Cachorro" />
+        <nav>
+          <a href="./">Features</a>
+          <a href="./">Team</a>
+          <a href="">Sign In</a>
+        </nav>
+      </header>
+    );
+  };
+  
+  export default cabecalho;
---
+++ fylo-dark/src/components/main/Principal.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+const principal = () => {
+    return(
+        <main>
+
+        </main>
+    );
+}
+
+export default principal;
