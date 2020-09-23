c60de035b46a950f7ffac823b92198cf38d95e2b
Lucca Hora <luccahora@gmail.com>
2020-09-23 01:31:12 -0300

    header

--- fylo-dark/src/App.js
+++ fylo-dark/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
-import './assets/main.css'
-import Cabecalho from './components/header/Cabecalho'
-import Principal from './components/main/Principal'
+import React from "react";
+import "./App.css";
+import "./assets/main.css";
+import Cabecalho from "./components/header/Cabecalho";
+import Principal from "./components/main/Principal";
 
 function App() {
   return (
     <div className="App">
-     <Cabecalho/>
-     <Principal/>
+      <Cabecalho />
+      <Principal />
     </div>
   );
 }
